edabf93e0ce3feb7013bf45b073746fa1ca5d37d
Leo Lanese <developer@leolanese.com>
2015-06-15 21:18:13 +0100

    few extra interesting functions
    borrowed few functions from davidwalsh.name recommendation

--- angularUtilityService.js
+++ angularUtilityService.js
@@ -103,25 +103,73 @@
             return obj instanceof $;
         },
         
+        /* borrowed from davidwalsh.name */
+        // Returns a function, that, as long as it continues to be invoked, will not
+        // be triggered. The function will be called after it stops being called for
+        // N milliseconds. If `immediate` is passed, trigger the function on the
+        // leading edge, instead of the trailing.
         function debounce(func, wait, immediate) {
-            var timeout, result;
-            return function() {
-                var context = this, args = arguments, later, callNow;
-                later = function() {
-                    timeout = null;
-                    if (!immediate) {
-                        result = func.apply(context, args);
-                    }
-                };
-                callNow = immediate && !timeout;
-                clearTimeout(timeout);
-                timeout = setTimeout(later, wait);
-                if (callNow) {
-                    result = func.apply(context, args);
-                }
-                return result;
-            };
+        	var timeout;
+        	return function() {
+        		var context = this, args = arguments;
+        		var later = function() {
+        			timeout = null;
+        			if (!immediate) func.apply(context, args);
+        		};
+        		var callNow = immediate && !timeout;
+        		clearTimeout(timeout);
+        		timeout = setTimeout(later, wait);
+        		if (callNow) func.apply(context, args);
+        	};
+        };
+        
+        /* Usage
+        var myEfficientFn = debounce(function() {
+        	// All the taxing stuff you do
+        }, 250);
+        window.addEventListener('resize', myEfficientFn);
+        */
+        
+        // The once function ensures a given function can only be called once, thus prevent duplicate initialization
+        function once(fn, context) { 
+        	var result;
+        
+        	return function() { 
+        		if(fn) {
+        			result = fn.apply(context || this, arguments);
+        			fn = null;
+        		}
+        
+        		return result;
+        	};
         }
+        
+        // Usage
+        /*
+        var canOnlyFireOnce = once(function() {
+        	console.log('Fired!');
+        });
+        canOnlyFireOnce(); // "Fired!"
+        canOnlyFireOnce(); // nada
+        */
+        
+        // Getting an absolute URL from a variable string
+        var getAbsoluteUrl = function() {
+        	var a;
+        
+        	return function(url) {
+        		if(!a) a = document.createElement('a');
+        		a.href = url;
+        
+        		return a.href;
+        	};
+        });
+        
+        // Usage
+        /*
+        getAbsoluteUrl('/something'); // http://davidwalsh.name/something
+        */
+
         
         function toStr(s) {
             return _.isUndefined(s) || s === null ? "" : s + "";
